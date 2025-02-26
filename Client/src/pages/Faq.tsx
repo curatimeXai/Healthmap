5e7e0dea21e3c54b90d26566da512ff74110f904
Carinalak <carina.lakosil@gmail.com>
2025-02-26 13:05:03 +0100

    :construction: Changed Faq text

--- Client/src/pages/Faq.tsx
+++ Client/src/pages/Faq.tsx
@@ -8,29 +8,30 @@ return(
 <H1RedTitle>FAQ</H1RedTitle>
 <WhiteWrapper>
   <TextBoxLongTransparent>
-    <H2NewsTitle>How does the 'Ai WhatIf' tool help improve heart health?</H2NewsTitle> 
-    The 'Ai Whatif' tool provides insights into how changes in lifestyle or medical treatments might impact your heart health. By simulating different scenarios and analyzing their potential outcomes, the tool helps you understand how various factors such as diet, exercise, and medication-can influence your cardiovascular health. This personalized approach allows you to make informed decisions about your health management. Keep in mind that while the tool offers valuable insights, it is intended for informational purposes only and should not replace professional medical advice.
+    <H2NewsTitle>Question</H2NewsTitle> 
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non, recusandae commodi cum eligendi repellat distinctio cumque nesciunt qui, illo laborum similique, velit unde suscipit id at fugiat facere tenetur eaque!
   </TextBoxLongTransparent>
 </WhiteWrapper>
 
 <WhiteWrapper>
   <TextBoxLongTransparent>
-    <H2NewsTitle>What did the FINEARTS-HF trial reveal about finerenone's effect on heart failure and cardiovascular death?</H2NewsTitle> 
-    The trial showed that finerenone reduced heart failure events and cardiovascular death in patients with mildly reduced or preserved ejection fraction. Benefits were seen across different ejection fractions and existing therapies, though finerenone increased the risk of hyperkalemia.
+    <H2NewsTitle>Question</H2NewsTitle> 
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ipsam, quod adipisci cumque accusamus doloribus nulla blanditiis numquam cupiditate assumenda alias quisquam nam aspernatur deleniti dignissimos et similique inventore quae?
   </TextBoxLongTransparent>
 </WhiteWrapper>
 
 <WhiteWrapper>
   <TextBoxLongTransparent>
-    <H2NewsTitle>Why is it important to care about one's heart health?</H2NewsTitle> 
-    If you want to have a healthy heart, it is important to focus on several key areas related to lifestyle and health habits. Main factors that help keep the heart healthy are sufficient sleep, a balanced diet rich in fruits and vegetables, healthy fats, reduced salt intake, limited processed foods, regular physical activity, controling one's cholesterol level and avoiding smoking and alcohol.
+    <H2NewsTitle>Question</H2NewsTitle> 
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere vel neque impedit soluta sapiente minus? Fugit neque nemo omnis quam. Assumenda fuga culpa mollitia optio velit nesciunt voluptatibus fugiat voluptatum?
     </TextBoxLongTransparent>
 </WhiteWrapper>
 
 <WhiteWrapper>
   <TextBoxLongTransparent>
-    <H2NewsTitle>What are the WebAPPs/Projects ?</H2NewsTitle> 
-    The projects are web applications that allows you to learn about heart diseases by testing your heart's health. Dependending on the application, you can just answer questions, or add an image/echography of your heart to get a diagnostic made by one of our Al. Keep in mind that these diagnostics might not be correct and that you should check with a doctor in case of doubts/problems about your heart.    </TextBoxLongTransparent>
+    <H2NewsTitle>Question</H2NewsTitle> 
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni vel laudantium maiores dolores dolor porro blanditiis sit exercitationem, aspernatur explicabo, praesentium velit at necessitatibus deserunt nesciunt aliquam, quod dolore? Soluta.
+  </TextBoxLongTransparent>
 </WhiteWrapper>
 
 </>)
